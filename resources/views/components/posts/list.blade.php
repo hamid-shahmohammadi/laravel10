46e566188dab38ae5c7e7643314e96e4c6eb5c16
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-04-03 14:18:25 +0430

    5.2

--- resources/views/components/posts/list.blade.php
+++ resources/views/components/posts/list.blade.php
@@ -1,24 +1,36 @@
-<div>
+<div x-data="setup()">
     <h2>Posts</h2>
-    <div x-data="setup()">
-        <template x-for="post in posts">
+    <input x-model="search" placeholder="Search...">
+    <div>
+        <template x-for="post in searchItem" :key="post.id">
             <li x-text="`${post.id} ${post.title}`"></li>
         </template>
-        <button class="bg-blue-500 text-white py-1 px-2 rounded-md disabled:bg-blue-200" @click="getPost()" :disabled="nextPageUrl===null">more</button>
+        <button class="bg-blue-500 text-white py-1 px-2 rounded-md disabled:bg-blue-200" @click="getPost()"
+            :disabled="nextPageUrl === null">more</button>
     </div>
 
     <script>
         function setup() {
             return {
+                search: "",
                 posts: [],
-                nextPageUrl:null,
+                nextPageUrl: null,
                 init() { // mounted vue
                     this.getPost();
                 },
-
-
+                get searchItem() {
+                    if (this.search === "") {
+                        return this.posts;
+                    }
+                    return this.posts.filter((item) => {
+                        console.log(item)
+                        return item.title
+                            .toLowerCase()
+                            .includes(this.search.toLowerCase());
+                    });
+                },
                 getPost() {
-                    const self=this;
+                    const self = this;
                     let url = self.nextPageUrl ?? "{{ route('api.posts') }}"
                     axios.get(url, {
                         headers: {
@@ -27,8 +39,8 @@
                         },
                     }).then((res) => {
 
-                        self.posts=self.posts.concat(res.data.posts.data);
-                        self.nextPageUrl=res.data.posts.next_page_url;
+                        self.posts = self.posts.concat(res.data.posts.data);
+                        self.nextPageUrl = res.data.posts.next_page_url;
                         console.log(self.posts)
                     }).catch((err) => console.log(err));
                 }
