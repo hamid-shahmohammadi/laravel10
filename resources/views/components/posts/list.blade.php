a9fc04796348d0ba60ee0e96273e73b25b6b68cc
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-04-09 10:44:35 +0430

    01-01-20-1044

--- resources/views/components/posts/list.blade.php
+++ resources/views/components/posts/list.blade.php
@@ -1,50 +1,52 @@
-<div x-data="setup()">
+<div x-data="listPost()">
     <h2>Posts</h2>
-    <input x-model="search" placeholder="Search...">
-    <div>
+    <input x-model="search" placeholder="Search..." class="dark:bg-gray-600 my-2 px-1 rounded-md">
+    <div class="py-2">
         <template x-for="post in searchItem" :key="post.id">
             <li x-text="`${post.id} ${post.title}`"></li>
         </template>
-        <button class="bg-blue-500 text-white py-1 px-2 rounded-md disabled:bg-blue-200" @click="getPost()"
+        <button class="bg-blue-500 text-white py-1 px-2 rounded-md disabled:bg-blue-200 mt-2" @click="getPost()"
             :disabled="nextPageUrl === null">more</button>
     </div>
-
-    <script>
-        function setup() {
-            return {
-                search: "",
-                posts: [],
-                nextPageUrl: null,
-                init() { // mounted vue
-                    this.getPost();
-                },
-                get searchItem() {
-                    if (this.search === "") {
-                        return this.posts;
-                    }
-                    return this.posts.filter((item) => {
-                        console.log(item)
-                        return item.title
-                            .toLowerCase()
-                            .includes(this.search.toLowerCase());
-                    });
-                },
-                getPost() {
-                    const self = this;
-                    let url = self.nextPageUrl ?? "{{ route('api.posts') }}"
-                    axios.get(url, {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': 'Bearer ' + localStorage.getItem("token")
-                        },
-                    }).then((res) => {
-                        console.log(res)
-                        self.posts = self.posts.concat(res.data.data);
-                        self.nextPageUrl = res.data.links.next;
-                        console.log(self.posts)
-                    }).catch((err) => console.log(err));
-                }
-            }
-        }
-    </script>
 </div>
+
+<script>
+
+listPost = () => {
+    return {
+        search: "",
+        posts: [],
+        nextPageUrl: null,
+        init() { // mounted vue
+            this.getPost();
+        },
+        get searchItem() {
+            if (this.search === "") {
+                return this.posts;
+            }
+            return this.posts.filter((item) => {
+                console.log(item)
+                return item.title
+                    .toLowerCase()
+                    .includes(this.search.toLowerCase());
+            });
+        },
+        getPost() {
+            const self = this;
+            let url = self.nextPageUrl ?? "{{ route('api.posts') }}"
+            axios.get(url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + this.$store.post.token
+                },
+            }).then((res) => {
+                console.log(res)
+                self.posts = self.posts.concat(res.data.data);
+                self.nextPageUrl = res.data.links.next;
+                console.log(self.posts)
+            }).catch((err) => console.log(err));
+        }
+    }
+}
+
+</script>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,15 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-posts.create  />
-                    <hr class="my-4"/>
-                    <x-posts.list  />
-
+                    <x-posts.create />
+                    <hr class="my-4" />
+                    <x-posts.list />
                 </div>
             </div>
         </div>
     </div>
     <script>
-        localStorage.setItem("token", "{{ $token }}");
+        document.addEventListener('alpine:init', () => {
+            Alpine.store('post', {
+                token: "{{ $token }}",
+
+            })
+        })
     </script>
 </x-app-layout>
