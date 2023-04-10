eb942c0fe422646e09c5ecd0cddcc8b87217aa14
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-04-10 11:34:18 +0430

    6.darkmode

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,19 @@ import './bootstrap';
 import 'flowbite';
 import Alpine from 'alpinejs';
 
+Alpine.store('darkMode', {
+    dark: false,
+
+    toggle() {
+        this.dark = ! this.dark
+        if(this.dark){
+            document.documentElement.classList.add('dark')
+        }else{
+            document.documentElement.classList.remove('dark')
+        }
+    }
+})
+
 window.Alpine = Alpine;
 
 Alpine.start();
